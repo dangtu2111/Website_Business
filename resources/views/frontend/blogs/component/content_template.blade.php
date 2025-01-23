0628a75cce6dccceade01da1efc2db5dd52d31d3
Dang Tu <anhtuhanam1@gmail.com>
2025-01-23 23:38:21 +0700

    update

--- resources/views/frontend/blogs/component/content_template.blade.php
+++ resources/views/frontend/blogs/component/content_template.blade.php
@@ -12,19 +12,19 @@
                                 <div class="row align-items-center">
                                     <div class="col-md-5 col-sm-4 mb30">
                                         <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/hoi_doanh_nghiep_quan_1_tang_cuong_vai_tro_cau_noi_giua_doanh_nghiep_va_chinh_quyen.html"
-                                                title="HỘI DOANH NGHIỆP QUẬN 1 TĂNG CƯỜNG VAI TRÒ CẦU NỐI GIỮA DOANH NGHIỆP VÀ CHÍNH QUYỀN">
+                                            <a href="{{ config('app.url') . '/client/post/' . ($post->category->slug??"null") . '/' . $post->slug }}"
+                                                title="{{$post->title}}">
                                                 <img width="380" height="250"
                                                     src="{{$post->cover_image}}"
                                                     class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="HỘI DOANH NGHIỆP QUẬN 1 TĂNG CƯỜNG VAI TRÒ CẦU NỐI GIỮA DOANH NGHIỆP VÀ CHÍNH QUYỀN">
+                                                    alt="{{$post->title}}">
 
                                             </a>
                                         </div>
                                     </div>
                                     <div class="col-md-7 col-sm-8 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/hoi_doanh_nghiep_quan_1_tang_cuong_vai_tro_cau_noi_giua_doanh_nghiep_va_chinh_quyen.html"
-                                            title="HỘI DOANH NGHIỆP QUẬN 1 TĂNG CƯỜNG VAI TRÒ CẦU NỐI GIỮA DOANH NGHIỆP VÀ CHÍNH QUYỀN">
+                                        <a href="{{ config('app.url') . '/client/post/' . ($post->category->slug??"null") . '/' . $post->slug }}"
+                                            title="{{$post->title}}">
                                             <h4 class="title">{{$post->title}}</h4>
                                         </a>
                                         <div class="meta mb10">
@@ -51,121 +51,33 @@
                     <div id="secondary" class="widget-area col-sm-12 col-md-4" role="complementary">
                         <aside class="widget post-list">
                             <h2 class="ldtl-title">Tin mới nhất</h2>
+                            @if(isset($posts))
+                            @foreach($posts as $post)
                             <div id="post-f4c2563a-c254-4bb7-9111-72f2d68e9b45"
                                 class="post-item post-sidebar-item post-f4c2563a-c254-4bb7-9111-72f2d68e9b45 post type-post status-publish format-standard has-post-thumbnail hentry category-tin-tuc">
                                 <div class="row align-items-center">
                                     <div class="col-md-5 col-sm-12 mb30">
                                         <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/ong-phan-thanh-tan-chu-tich-hoi-doanh-nghiep-quan-1-dong-gop-tao-gia-tri-khong-cho-doi-nhan-gia-tri.html"
-                                                title="ÔNG PHAN THÀNH TÂN - CHỦ TỊCH HỘI DOANH NGHIỆP QUẬN 1: ĐÓNG GÓP TẠO GIÁ TRỊ, KHÔNG CHỜ ĐỢI NHẬN GIÁ TRỊ">
+                                            <a href="{{ config('app.url') . '/client/post/' . ($post->category->slug??"null") . '/' . $post->slug }}"
+                                                title="{{$post->title}}">
                                                 <img width="380" height="250"
-                                                    src="images/1_1732685071_5713759426319965.jpg"
+                                                    src="{{$post->cover_image}}"
                                                     class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="ÔNG PHAN THÀNH TÂN - CHỦ TỊCH HỘI DOANH NGHIỆP QUẬN 1: ĐÓNG GÓP TẠO GIÁ TRỊ, KHÔNG CHỜ ĐỢI NHẬN GIÁ TRỊ">
+                                                    alt="{{$post->title}}">
                                             </a>
                                         </div>
                                     </div>
                                     <div class="col-md-7 col-sm-12 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/ong-phan-thanh-tan-chu-tich-hoi-doanh-nghiep-quan-1-dong-gop-tao-gia-tri-khong-cho-doi-nhan-gia-tri.html"
-                                            title="ÔNG PHAN THÀNH TÂN - CHỦ TỊCH HỘI DOANH NGHIỆP QUẬN 1: ĐÓNG GÓP TẠO GIÁ TRỊ, KHÔNG CHỜ ĐỢI NHẬN GIÁ TRỊ">
-                                            <h4 class="title">ÔNG PHAN THÀNH TÂN - CHỦ TỊCH HỘI DOANH NGHIỆP QUẬN 1:
-                                                ĐÓNG GÓP TẠO GIÁ TRỊ, KHÔNG CHỜ ĐỢI NHẬN GIÁ TRỊ</h4>
+                                        <a href="{{ config('app.url') . '/client/post/' . ($post->category->slug??"null") . '/' . $post->slug }}"
+                                            title="{{$post->title}}">
+                                            <h4 class="title">{{$post->title}}</h4>
                                         </a>
                                     </div>
                                 </div>
                             </div>
-                            <div id="post-f89c0777-1335-4337-abcb-963aa655330b"
-                                class="post-item post-sidebar-item post-f89c0777-1335-4337-abcb-963aa655330b post type-post status-publish format-standard has-post-thumbnail hentry category-tin-tuc">
-                                <div class="row align-items-center">
-                                    <div class="col-md-5 col-sm-12 mb30">
-                                        <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/chia-se-cach-giai-quyet-bai-toan-kinh-doanh-cho-nguoi-moi-bat-dau.html"
-                                                title="CHIA SẺ CÁCH GIẢI QUYẾT BÀI TOÁN KINH DOANH CHO NGƯỜI MỚI BẮT ĐẦU - HỘI DOANH NGHIỆP QUẬN 1">
-                                                <img width="380" height="250"
-                                                    src="images/463224982_122112653222552284_5718418662490201517_n_1730696993_4527597840605804.jpg"
-                                                    class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="CHIA SẺ CÁCH GIẢI QUYẾT BÀI TOÁN KINH DOANH CHO NGƯỜI MỚI BẮT ĐẦU - HỘI DOANH NGHIỆP QUẬN 1">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7 col-sm-12 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/chia-se-cach-giai-quyet-bai-toan-kinh-doanh-cho-nguoi-moi-bat-dau.html"
-                                            title="CHIA SẺ CÁCH GIẢI QUYẾT BÀI TOÁN KINH DOANH CHO NGƯỜI MỚI BẮT ĐẦU - HỘI DOANH NGHIỆP QUẬN 1">
-                                            <h4 class="title">CHIA SẺ CÁCH GIẢI QUYẾT BÀI TOÁN KINH DOANH CHO NGƯỜI MỚI
-                                                BẮT ĐẦU - HỘI DOANH NGHIỆP QUẬN 1</h4>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="post-d7afd394-5057-4664-8c17-6168895c0319"
-                                class="post-item post-sidebar-item post-d7afd394-5057-4664-8c17-6168895c0319 post type-post status-publish format-standard has-post-thumbnail hentry category-tin-tuc">
-                                <div class="row align-items-center">
-                                    <div class="col-md-5 col-sm-12 mb30">
-                                        <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/cach-tham-gia-hoi-vien-doanh-nghiep-quan-1.html"
-                                                title="CÁCH THAM GIA HỘI VIÊN HỘI DOANH NGHIỆP QUẬN 1 - CÙNG TẠO BƯỚC ĐỆM VỮNG CHẮC CHO DOANH NGHIỆP CỦA BẠN">
-                                                <img width="380" height="250"
-                                                    src="images/%C4%90K%20THAM%20GIA%20HV%20HDNQ1_1730696139_1747388822233363.jpg"
-                                                    class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="CÁCH THAM GIA HỘI VIÊN HỘI DOANH NGHIỆP QUẬN 1 - CÙNG TẠO BƯỚC ĐỆM VỮNG CHẮC CHO DOANH NGHIỆP CỦA BẠN">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7 col-sm-12 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/cach-tham-gia-hoi-vien-doanh-nghiep-quan-1.html"
-                                            title="CÁCH THAM GIA HỘI VIÊN HỘI DOANH NGHIỆP QUẬN 1 - CÙNG TẠO BƯỚC ĐỆM VỮNG CHẮC CHO DOANH NGHIỆP CỦA BẠN">
-                                            <h4 class="title">CÁCH THAM GIA HỘI VIÊN HỘI DOANH NGHIỆP QUẬN 1 - CÙNG TẠO
-                                                BƯỚC ĐỆM VỮNG CHẮC CHO DOANH NGHIỆP CỦA BẠN</h4>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="post-852da8a0-1a1d-429f-82fe-7f9cb7034b11"
-                                class="post-item post-sidebar-item post-852da8a0-1a1d-429f-82fe-7f9cb7034b11 post type-post status-publish format-standard has-post-thumbnail hentry category-tin-tuc">
-                                <div class="row align-items-center">
-                                    <div class="col-md-5 col-sm-12 mb30">
-                                        <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/ra_mat_ban_chap_hanh_hoi_doanh_nghiep_quan_1_nhiem_ky_2024_2029.html"
-                                                title="RA MẮT BAN CHẤP HÀNH HỘI DOANH NGHIỆP QUẬN 1 NHIỆM KỲ 2024 - 2029">
-                                                <img width="380" height="250"
-                                                    src="images/20241014_DAIHOIHDNQ1WEB118_1728986584_1505860839835270.jpg"
-                                                    class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="RA MẮT BAN CHẤP HÀNH HỘI DOANH NGHIỆP QUẬN 1 NHIỆM KỲ 2024 - 2029">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7 col-sm-12 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/ra_mat_ban_chap_hanh_hoi_doanh_nghiep_quan_1_nhiem_ky_2024_2029.html"
-                                            title="RA MẮT BAN CHẤP HÀNH HỘI DOANH NGHIỆP QUẬN 1 NHIỆM KỲ 2024 - 2029">
-                                            <h4 class="title">RA MẮT BAN CHẤP HÀNH HỘI DOANH NGHIỆP QUẬN 1 NHIỆM KỲ 2024
-                                                - 2029</h4>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="post-8f917ce3-29d3-485a-ad75-fb5a197f1c69"
-                                class="post-item post-sidebar-item post-8f917ce3-29d3-485a-ad75-fb5a197f1c69 post type-post status-publish format-standard has-post-thumbnail hentry category-tin-tuc">
-                                <div class="row align-items-center">
-                                    <div class="col-md-5 col-sm-12 mb30">
-                                        <div class="thumb">
-                                            <a href="https://hoidoanhnghiepquan1.com/dai_hoi_hoi_daonh_nghiep_quan_1_lan_thu_3_nhiem_ky_2024_2029_dien_ra_thanh_cong.html"
-                                                title="ĐẠI HỘI HỘI DOANH NGHIỆP QUẬN 1 LẦN THỨ III, NHIỆM KỲ 2024 - 2029 DIỄN RA THÀNH CÔNG">
-                                                <img width="380" height="250"
-                                                    src="images/%E1%BA%A2nh%208_1728985944_0666304382124338.jpg"
-                                                    class="attachment-380x250 size-380x250 wp-post-image"
-                                                    alt="ĐẠI HỘI HỘI DOANH NGHIỆP QUẬN 1 LẦN THỨ III, NHIỆM KỲ 2024 - 2029 DIỄN RA THÀNH CÔNG">
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7 col-sm-12 mb30">
-                                        <a href="https://hoidoanhnghiepquan1.com/dai_hoi_hoi_daonh_nghiep_quan_1_lan_thu_3_nhiem_ky_2024_2029_dien_ra_thanh_cong.html"
-                                            title="ĐẠI HỘI HỘI DOANH NGHIỆP QUẬN 1 LẦN THỨ III, NHIỆM KỲ 2024 - 2029 DIỄN RA THÀNH CÔNG">
-                                            <h4 class="title">ĐẠI HỘI HỘI DOANH NGHIỆP QUẬN 1 LẦN THỨ III, NHIỆM KỲ 2024
-                                                - 2029 DIỄN RA THÀNH CÔNG</h4>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
+                            @endif
+                            
                         </aside>
                         <aside class="widget">
                             <h2 class="ldtl-title">Menu chính</h2>
