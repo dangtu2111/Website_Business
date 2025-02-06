3d753e88febabdcd3c5f7252eafb04fec5549c85
Dang Tu <anhtuhanam1@gmail.com>
2025-02-06 15:40:06 +0700

    update

--- resources/views/frontend/component/content/banner.blade.php
+++ resources/views/frontend/component/content/banner.blade.php
@@ -1,5 +1,5 @@
 <?php
-  $url_image = $category->cover_image ?? (config('app.url') . '/Frontend/fac8dcafc7346ea0ee5034d36f8734d1/wp-content/uploads/1900/2024/10/11/cover_1728639357_6826272568245068.jpg');
+  $url_image = $category->cover_image ?? (config('app.url') . '/Backend/img/background001.png');
 
 ?>
 <section class="breadcrumbs-section background_bg header-bg"
